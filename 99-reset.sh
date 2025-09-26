eb970aa707cfba1ebeadc7c3997e86e3cfaa1a81
mnakagaw <mnakagaw@redhat.com>
2025-09-26 18:29:36 +0900

    模範解答群を追加

---
+++ 99-reset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git reset --hard HEAD
+
+echo ""
+echo "======================================"
+echo "Source code reset to initial state."
+echo "======================================"
---
+++ zz_coolstore_example/src/main/java/com/redhat/coolstore/rest/RestApplication.java
@@ -0,0 +1,8 @@
+// This file should be removed.
+//
+// In Quarkus, JAX-RS activation is automatic, and this class is no longer necessary.
+// The application's root path, which was defined here as "/services",
+// should be configured in the `src/main/resources/application.properties` file by adding the following line:
+//
+// quarkus.resteasy-reactive.path=/services
+//
