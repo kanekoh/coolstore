d1b8d96279df5584cae525c9ace130a45916e5a4
mnakagaw <mnakagaw@redhat.com>
2025-10-03 13:12:11 +0900

    Revert "模範解答群を追加"
    This reverts commit eb970aa707cfba1ebeadc7c3997e86e3cfaa1a81.

--- zz_coolstore_example/src/main/java/com/redhat/coolstore/rest/RestApplication.java
+++
@@ -1,8 +0,0 @@
-// This file should be removed.
-//
-// In Quarkus, JAX-RS activation is automatic, and this class is no longer necessary.
-// The application's root path, which was defined here as "/services",
-// should be configured in the `src/main/resources/application.properties` file by adding the following line:
-//
-// quarkus.resteasy-reactive.path=/services
-//
